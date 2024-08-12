bed41f3fe103f90286b52268c20daef258384c75
Marcus Benoit <benoit.marcus@gmail.com>
2024-08-12 12:41:13 +0200

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Marcus Benoit on 12.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
